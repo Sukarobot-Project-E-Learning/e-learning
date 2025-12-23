5fdfd9eb39dd1f5e499b95f1f73da09121f91acf
rahmanditya <rahmanditya35@gmail.com>
2025-12-23 15:03:37 +0700

    build npm

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'E-Learning Admin') - Sukarobot</title>
-
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 
