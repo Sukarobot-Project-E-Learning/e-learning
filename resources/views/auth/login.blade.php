a85d62b50c94cb8cda6a6a3d4038135a7a785b7d
gilarwahibul <gilarwahibul@gmail.com>
2026-01-23 10:41:47 +0700

    wwwww

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,26 @@
         rel="stylesheet"
     />
     
-    <!-- Vite CSS & JS -->
+    <!-- TailwindCSS CDN Fallback (in case Vite assets fail to load) -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        orange: {
+                            50: '#fff7ed', 100: '#ffedd5', 200: '#fed7aa', 300: '#fdba74',
+                            400: '#fb923c', 500: '#f97316', 600: '#ea580c', 700: '#c2410c',
+                            800: '#9a3412', 900: '#7c2d12', 950: '#431407'
+                        }
+                    }
+                }
+            }
+        }
+    </script>
+    
+    <!-- Vite CSS & JS (will override CDN if loaded successfully) -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <!-- Alpine.js x-cloak -->
