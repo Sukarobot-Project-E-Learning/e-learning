d3afdd2e29b573f6e825e2574012c52c16642e6c
ivan <sudahdigunakan07@gmail.com>
2026-02-13 15:14:29 +0700

    konfigurasi artsaos/seotools

--- resources/views/client/main.blade.php
+++ resources/views/client/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Sukarobot Academy</title>
+    {!! SEO::generate() !!}
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="{{  asset('assets/elearning/client/css/partials/navbar.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
